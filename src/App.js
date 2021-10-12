763900c0eafccb1e7d42e67bd1c2c5eac570866d
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2021-10-12 13:54:28 +0300

    added props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
 
+
+ const Box = (props)=>{
+   return (
+     <div className="box">
+      <div>
+        <h2>{props.name}</h2> 
+        <p>Title: {props.title}  </p>  
+        <p>Age: {props.age} </p> 
+      </div>
+     </div>
+   )
+ }
 function App() {
   return (
     <div className="App">
       <div>
-      <h1>Hello world boxes demo</h1>
-      <h1>Hello world boxes demo</h1>
-      </div>
-      <div>
-      <h1>Hello world boxes demo</h1>
-      <h1>Hello world boxes demo</h1>
+      <header>
+        <h1>Logo</h1>  
+    </header>
+    <main>
+    <Box name = "Maria" title = "CEO" age = "29"/>
+    <Box name = "kati" title = "Developer" age = "25"/>
+    <Box name = "Kartin" title = "Designer" age = "45"/>  
+    </main>
+    <footer>
+        <p>
+            <small>
+            Copyrights
+            </small>
+        </p>
+        
+        </footer>
       </div>
     </div>
   );
