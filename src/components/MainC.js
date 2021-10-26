6bb18113817b27cf778ea443b5cad113ac5d9ce8
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2021-10-26 13:16:32 +0300

    Map function added

--- src/components/MainC.js
+++ src/components/MainC.js
@@ -5,9 +5,9 @@ import Box from "./Box";
 class MainC extends Component{
     state = {
       persons:[
-        { name : "Maria", title: "CEO", age:29},
-        {  name : "Kati", title: "Developer", age:25},
-         {  name : "Kartin", title: "designer", age:45},
+        {id:1 , name : "Maria", title: "CEO", age:29},
+        { id:2, name : "Kati", title: "Developer", age:25},
+         { id:3, name : "Kartin", title: "designer", age:45},
       ]
     };
 
@@ -19,7 +19,13 @@ class MainC extends Component{
         return (
           <>
         <main>
-            <Box  
+          {/* list item in array  */}
+          {this.state.persons.map((item)=><Box 
+          key={item.id} 
+          name = {item.name} 
+          age ={item.age} 
+          title={item.title}/>)}
+            {/* <Box  
             name={this.state.persons[0].name}
             title={this.state.persons[0].title}
             age={this.state.persons[0].age}
@@ -34,7 +40,12 @@ class MainC extends Component{
             name={this.state.persons[2].name}
             title={this.state.persons[2].title}
             age={this.state.persons[2].age}
-            />
+            /> */}
+
+            <ul>
+
+              {this.state.persons.map(item => <li key={item.id}> { item.name}is {item.age} old and is {item.title}</li>)}
+            </ul>
   
         </main>
         <button onClick={this.changeHandler}>click me</button>
